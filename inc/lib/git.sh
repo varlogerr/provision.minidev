5b6822c9bbacf7a0ca8620f46a75071c85ed3d8e
Varlog Err <varlog.err@gmail.com>
2022-06-12 04:36:15 +0300

    fix bashrc.d directory path in bashrc

--- inc/lib/git.sh
+++ inc/lib/git.sh
@@ -10,19 +10,19 @@ git.gitconfig() {
 }
 
 git.config_ps1() {
-  local dest="${CONF[target_user_home]}/${SYS_CONF[bashrcd_prefix]}/0100-git-ps1.sh"
-  local dest_dir="$(dirname "${dest}")"
-  local source_cmd="for f in '${dest_dir}'/*.sh; do . \"${f}\"; done"
+  local confd="${CONF[target_user_home]}/${SYS_CONF[bashrcd_prefix]}"
+  local dest_file="${CONF[target_user_home]}/${SYS_CONF[bashrcd_prefix]}/0100-git-ps1.sh"
+  local source_cmd="for f in '${confd}'/*.sh; do . \"\${f}\" > /dev/null; done"
   local bashrc_path="${CONF[target_user_home]}/.bashrc"
 
   common.log "git:config" "configuring git PS1 ..."
 
-  su - "${CONF[target_user]}" -c "mkdir -p '${dest_dir}'"
-  su - "${CONF[target_user]}" -c "cp '${PROJECT_DIR}/inc/tpl/git/ps1.sh' '${dest}'"
+  su - "${CONF[target_user]}" -c "mkdir -p '${confd}'"
+  su - "${CONF[target_user]}" -c "cp '${PROJECT_DIR}/inc/tpl/git/ps1.sh' '${dest_file}'"
 
   ! grep -qF "${source_cmd}" "${bashrc_path}" \
-    && echo $'\n' "# source ${SYS_CONF[bashrcd_prefix]}/*.sh files" \
-      $'\n' "${source_cmd}" >> "${bashrc_path}"
+    && echo $'\n'"# source ${confd}/*.sh files" >> "${bashrc_path}" \
+    && echo "${source_cmd}" >> "${bashrc_path}"
 }
 
 git.gitconfig
