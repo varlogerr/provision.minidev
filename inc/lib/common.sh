2df7cd2c70ee76e6de49efb7828c3c91252150a2
Varlog Err <varlog.err@gmail.com>
2022-06-12 15:02:42 +0300

    install google chrome

--- inc/lib/common.sh
+++ inc/lib/common.sh
@@ -14,7 +14,7 @@ common.update_repository() {
 
 common.install_musts() {
   common.log "musts" "installing ..."
-  apt install -y wget curl vim
+  apt install -y curl gnupg2 vim wget
 }
 
 common.install_git() {
@@ -28,16 +28,39 @@ common.install_git() {
   apt install -y git
 }
 
+common.install_chrome() {
+  # in spiral 11 chrome doesn't start for some reason
+  return
+
+  local phase="chrome"
+  local gpg_file=/usr/share/keyrings/google-chrome.gpg
+  google-chrome-stable --version > /dev/null 2>&1 && {
+    common.log "${phase}" "skipping installation (installed)"
+    return
+  }
+
+  common.log "${phase}" "installing ..."
+  wget -q -O- https://dl.google.com/linux/linux_signing_key.pub \
+    | gpg --dearmor > "${gpg_file}"
+  echo "deb [arch=amd64 signed-by=${gpg_file}] http://dl.google.com/linux/chrome/deb/ stable main" \
+    > /etc/apt/sources.list.d/vscode.list
+  common.update_repository
+  apt install -y google-chrome-stable
+}
+
 common.install_vscode() {
   local phase="vscode"
+  local gpg_file=/usr/share/keyrings/microsoft.gpg
   which code > /dev/null && {
     common.log "${phase}" "skipping installation (installed)"
     return
   }
 
   common.log "${phase}" "installing ..."
-  wget -q "${CONF[ms_gpg_link]}" -O- | apt-key add -
-  add-apt-repository "deb [arch=amd64] ${CONF[vscode_repo_link]} stable main"
+  wget -q -O- "${CONF[ms_gpg_link]}" \
+    | gpg --dearmor > "${gpg_file}"
+  echo "deb [arch=amd64 signed-by=${gpg_file}] ${CONF[vscode_repo_link]} stable main" \
+    > /etc/apt/sources.list.d/vscode.list
   common.update_repository
   apt install -y code
 }
@@ -60,6 +83,7 @@ common.update_repository
 
 common.install_musts
 common.install_git
+common.install_chrome
 common.install_vscode
 common.config_vscode_editorconfig
 common.create_proj_dir
